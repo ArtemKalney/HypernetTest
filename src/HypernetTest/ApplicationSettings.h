f91440eb71d7cc9f87c71f72b76b82ec34691413
COMPUKTER\Compucter <artem.kalnei@gmail.com>
2021-08-30 20:27:57 +0700

    update optimization tests

--- src/HypernetTest/ApplicationSettings.h
+++ src/HypernetTest/ApplicationSettings.h
@@ -27,5 +27,5 @@
 #define IS_RANDOM_HYPERNET 0
 #define RANDOM_ADDITIONAL_EDGES 5
 // параметры для кумулятивных оценок
-#define IS_CUMULATIVE_MODE 1
+#define IS_CUMULATIVE_MODE 0
 #define REQUIRED_VALUE 12
