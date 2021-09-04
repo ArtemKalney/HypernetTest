86fc0af74938dd03160b877f74dc769602d046de
COMPUKTER\Compucter <artem.kalnei@gmail.com>
2021-09-04 18:52:13 +0700

    Revert "remove cumulative mode settings"
    This reverts commit 63b191d

---
+++ src/OptimizationTest/FullEnumerationAlgorithm.cpp
@@ -0,0 +1,30 @@
+#include "FullEnumerationAlgorithm.h"
+#include "../HypernetModel/Helpers/DataHelper.h"
+
+std::shared_ptr<Model> FullEnumerationAlgorithm::GetMinModel() {
+    std::vector<std::vector<Branch>> combinations;
+    std::vector<Branch> vector;
+    for (int i = 1; i < MAX_BRANCH_COUNT; ++i) {
+        vector.clear();
+        ComputeCombinations(_hypernet.GetFN(), combinations, vector, 0, i);
+    }
+    std::vector<Model> models;
+    for(auto &solution : combinations) {
+        auto model = new Model(_hypernet, solution);
+        if (model->CheckConditions()) {
+            CheckedConditions++;
+            models.push_back(*model);
+        } else {
+            UncheckedConditions++;
+        }
+        if (IS_DEBUG == 1) {
+            model->PrintModel();
+        }
+    }
+    if (models.empty()) {
+        throw std::runtime_error("There ara no models satisfying the task conditions. Please check model conditions.");
+    }
+    auto minModelIt = std::min_element(models.begin(), models.end(), Model::ModelCompare);
+
+    return std::make_shared<Model>(*minModelIt);
+}
---
+++ src/OptimizationTest/FullEnumerationAlgorithm.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../HypernetModel/Hypernet.h"
+#include "Model .h"
+
+class FullEnumerationAlgorithm {
+private:
+    H _hypernet;
+public:
+    FullEnumerationAlgorithm() = default;
+
+    FullEnumerationAlgorithm(H& hypernet) : _hypernet(hypernet) {}
+
+    std::shared_ptr<Model> GetMinModel();
+};
